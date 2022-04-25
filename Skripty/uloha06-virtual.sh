522fd236d8f2f5853d66a9476d130aec7455bbd7
Kevin Daněk <heindrich73@gmail.com>
2022-04-25 11:43:32 +0200

    Reorganizace

--- Skripty/uloha06-virtual.sh
+++ Skripty/uloha06-virtual.sh
@@ -1,63 +1,64 @@
-red=$(tput setaf 1)
-green=$(tput setaf 2)
-yellow=$(tput setaf 3)
-blue=$(tput setaf 4)
+#!/bin/bash
 
-normal=$(tput sgr0)
+################################################################################
+# Help                                                                         #
+################################################################################
+Help ()
+{
+   # Display Help
+   echo
+   echo "Skript pro konfigurace virtuálního stroje pro úlohu 6 podle instrukcí z Elearningu."
+   echo
+   echo "Syntax: ./uloha06-virtual.sh --help | interface_ip interface_dev router_ip"
+   echo
+   echo "Argumenty:"
+   echo "interface_ip       Vybraná IP, kterou má mít síťové rozhraní"
+   echo "interface_dev      Název rozhraní, kterému se připadí IP"
+   echo "router_ip          IP routeru"
+   echo
+   echo 'Příklad: ./uloha06-virtual.sh "192.168.1.60/24" "enp0s8" "192.168.1.1"'
+   echo
+}
 
+if [ $1 = "--help" ] 
+then
+    Help
+    exit
+fi
 
-interface_ip="192.168.1.60/24"
-interface_dev="enp0s8"
-router_ip="192.168.1.1"
+interface_ip=$1     # Např. "192.168.1.60/24"
+interface_dev=$2    # Např. "enp0s8"
+router_ip=$3        # Např. "192.168.1.1"
 
 # Vytvoření logu
 touch virtual.log
+
+# Povolení DHCP
+ifconfig $interface_dev dhcp start &>> virtual.log 
 
 #########################################
 #           Nastavení síťovky           #
 #########################################
 echo "Nastavení sekundářní síťové karty"
-
-echo -ne '[                      ]  (0%)\r'
 ip addr add $interface_ip dev $interface_dev &>> virtual.log
-
-echo -ne '[##########            ]  (50%)\r'
 ip route add default via $router_ip proto dhcp metric 90 &>> virtual.log
-
-echo -ne '[######################]  (100%)\r'
-echo -ne '\n\n'
-
 
 #####################################
 #           Setup Apache            #
 #####################################
 echo "Setupování Apache"
-echo -ne '[                      ]  (0%)\r'
 yum install httpd &>> /dev/null
-
-echo -ne '[#####                 ]  (33%)\r'
 service httpd start &>> virtual.log
-
-echo -ne '[###############       ]  (66%)\r'
 echo "Listen 5001" >> /etc/httpd/conf/httpd.conf
-
-echo -ne '[######################]  (100%)\r'
-echo -ne '\n\n'
-
 
 #####################################
 #           Vytvoření HTML          #
 #####################################
 
 echo "Vytvářím index.html"
-
-echo -ne '[                      ]  (0%)\r'
 touch /var/www/html/index.html &>> virtual.log
-
-echo -ne '[##########            ]  (33%)\r'
 chmod 777 /var/www/html -R &>> virtual.log
-
-echo -ne '[######################]  (66%)\r'
 printf "A%s" $(hostname | grep -Eo "[0-9]+") > /var/www/html/index.html
 
-echo -ne '[######################]  (100%)\r'
+echo "Done."
+echo "Protip: Přejdi do routeru a nastav tam přesměrování portů na každou stanici."
